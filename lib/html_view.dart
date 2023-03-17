beb80f9a584795e1ea7ea9d50b8dee01f0ff1431
Lane Brooks <dirjud@gmail.com>
2023-03-17 11:26:25 -0600

    updates for pub dep changes

--- lib/html_view.dart
+++ lib/html_view.dart
@@ -17,36 +17,56 @@ class _HtmlState extends State<HtmlView> {
   WebViewController? _controller;
   double? height;
 
+  @override void initState() {
+    super.initState();
+    _controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+    NavigationDelegate(
+      onProgress: (int progress) {
+        // Update loading bar.
+      },
+      onPageStarted: (String url) {},
+      onPageFinished: (String url) async {
+        if(_controller != null) {
+          try {
+            double h = (await _controller!.runJavaScriptReturningResult(
+                "document.documentElement.scrollHeight;")) as double;
+            setState(() {
+              height = h;
+            });
+          } catch(e, stack_trace) {
+            print(e);
+            print(stack_trace);
+          }
+        }
+      },
+      onWebResourceError: (WebResourceError error) {},
+      onNavigationRequest: (NavigationRequest req) {
+        canLaunch(req.url).then((result) {
+          if(result) {
+            launch(req.url);
+          }
+        });
+        return NavigationDecision.prevent;
+      },
+    ),
+  )
+  ..loadRequest(Uri.dataFromString(
+        widget.html,
+        mimeType: 'text/html',
+        encoding: Encoding.getByName('utf-8')
+    ));
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         height: height ?? 100.0,
-        child: WebView(
-          initialUrl: Uri.dataFromString(
-              widget.html,
-              mimeType: 'text/html',
-              encoding: Encoding.getByName('utf-8')
-          ).toString(),//'about:blank',
-          navigationDelegate: (req) {
-            canLaunch(req.url).then((result) {
-              if(result) {
-                launch(req.url);
-              }
-            });
-            return NavigationDecision.prevent;
-          },
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller = webViewController;
-          },
-          onPageFinished: (some) async {
-            if(_controller != null) {
-              double h = double.parse(await _controller!.evaluateJavascript("document.documentElement.scrollHeight;"));
-              setState(() {
-                height = h;
-              });
-            }
-          },
-          javascriptMode: JavascriptMode.unrestricted,
+        child: _controller == null ? Container(width:0) : WebViewWidget(
+          controller: _controller!,
         ));
   }
 
